8f8575bfa06fee82faf982a846e572db9e80d7a7
Kelly-James <kellyjamesclancy@gmail.com>
2018-12-10 10:03:10 -0800

    First Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 // GULP PACKAGES
 // Most packages are lazy loaded
-var gulp  = require('gulp'),
-    gutil = require('gulp-util'),
-    browserSync = require('browser-sync').create(),
-    filter = require('gulp-filter'),
-    touch = require('gulp-touch-cmd'),
-    plugin = require('gulp-load-plugins')();
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	browserSync = require('browser-sync').create(),
+	filter = require('gulp-filter'),
+	touch = require('gulp-touch-cmd'),
+	plugin = require('gulp-load-plugins')();
 
 
 // GULP VARIABLES
 // Modify these variables to match your project needs
 
 // Set local URL if using Browser-Sync
-const LOCAL_URL = 'http://jointswp-github.dev/';
+const LOCAL_URL = 'http://localhost:8888/costaVerde/';
 
 // Set path to Foundation files
 const FOUNDATION = 'node_modules/foundation-sites';
@@ -21,7 +21,7 @@ const FOUNDATION = 'node_modules/foundation-sites';
 const SOURCE = {
 	scripts: [
 		// Lets grab what-input first
-	    'node_modules/what-input/dist/what-input.js',
+		'node_modules/what-input/dist/what-input.js',
 
 		// Foundation core - needed if you want to use any of the components below
 		FOUNDATION + '/dist/js/plugins/foundation.core.js',
@@ -52,7 +52,7 @@ const SOURCE = {
 
 		// Place custom JS here, files will be concantonated, minified if ran with --production
 		'assets/scripts/js/**/*.js',
-    ],
+	],
 
 	// Scss files will be concantonated, minified if ran with --production
 	styles: 'assets/styles/scss/**/*.scss',
@@ -83,16 +83,16 @@ const JSHINT_CONFIG = {
 
 // GULP FUNCTIONS
 // JSHint, concat, and minify JavaScript
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
 
 	// Use a custom filter so we only lint custom JS
-	const CUSTOMFILTER = filter(ASSETS.scripts + 'js/**/*.js', {restore: true});
+	const CUSTOMFILTER = filter(ASSETS.scripts + 'js/**/*.js', { restore: true });
 
 	return gulp.src(SOURCE.scripts)
-		.pipe(plugin.plumber(function(error) {
-            gutil.log(gutil.colors.red(error.message));
-            this.emit('end');
-        }))
+		.pipe(plugin.plumber(function (error) {
+			gutil.log(gutil.colors.red(error.message));
+			this.emit('end');
+		}))
 		.pipe(plugin.sourcemaps.init())
 		.pipe(plugin.babel({
 			presets: ['es2015'],
@@ -100,77 +100,76 @@ gulp.task('scripts', function() {
 			ignore: ['what-input.js']
 		}))
 		.pipe(CUSTOMFILTER)
-			.pipe(plugin.jshint(JSHINT_CONFIG))
-			.pipe(plugin.jshint.reporter('jshint-stylish'))
-			.pipe(CUSTOMFILTER.restore)
+		.pipe(plugin.jshint(JSHINT_CONFIG))
+		.pipe(plugin.jshint.reporter('jshint-stylish'))
+		.pipe(CUSTOMFILTER.restore)
 		.pipe(plugin.concat('scripts.js'))
 		.pipe(plugin.uglify())
 		.pipe(plugin.sourcemaps.write('.')) // Creates sourcemap for minified JS
 		.pipe(gulp.dest(ASSETS.scripts))
+		.pipe(touch());
 });
 
 // Compile Sass, Autoprefix and minify
-gulp.task('styles', function() {
+gulp.task('styles', function () {
 	return gulp.src(SOURCE.styles)
-		.pipe(plugin.plumber(function(error) {
-            gutil.log(gutil.colors.red(error.message));
-            this.emit('end');
-        }))
+		.pipe(plugin.plumber(function (error) {
+			gutil.log(gutil.colors.red(error.message));
+			this.emit('end');
+		}))
 		.pipe(plugin.sourcemaps.init())
 		.pipe(plugin.sass())
 		.pipe(plugin.autoprefixer({
-		    browsers: [
-		    	'last 2 versions',
-		    	'ie >= 9',
+			browsers: [
+				'last 2 versions',
+				'ie >= 9',
 				'ios >= 7'
-		    ],
-		    cascade: false
+			],
+			cascade: false
 		}))
-		.pipe(plugin.cssnano({safe: true, minifyFontValues: {removeQuotes: false}}))
+		.pipe(plugin.cssnano({ safe: true, minifyFontValues: { removeQuotes: false } }))
 		.pipe(plugin.sourcemaps.write('.'))
 		.pipe(gulp.dest(ASSETS.styles))
-		.pipe(touch())
-		.pipe(browserSync.reload({
-          stream: true
-        }));
+		.pipe(touch());
 });
 
 // Optimize images, move into assets directory
-gulp.task('images', function() {
+gulp.task('images', function () {
 	return gulp.src(SOURCE.images)
 		.pipe(plugin.imagemin())
 		.pipe(gulp.dest(ASSETS.images))
+		.pipe(touch());
 });
 
- gulp.task( 'translate', function () {
-     return gulp.src( SOURCE.php )
-         .pipe(plugin.wpPot( {
-             domain: 'jointswp',
-             package: 'Example project'
-         } ))
-        .pipe(gulp.dest('file.pot'));
- });
+gulp.task('translate', function () {
+	return gulp.src(SOURCE.php)
+		.pipe(plugin.wpPot({
+			domain: 'jointswp',
+			package: 'Example project'
+		}))
+		.pipe(gulp.dest('file.pot'));
+});
 
 // Browser-Sync watch files and inject changes
-gulp.task('browsersync', function() {
+gulp.task('browsersync', function () {
 
-    // Watch these files
-    var files = [
-    	SOURCE.php,
-    ];
+	// Watch these files
+	var files = [
+		SOURCE.php,
+	];
 
-    browserSync.init(files, {
-	    proxy: LOCAL_URL,
-    });
+	browserSync.init(files, {
+		proxy: LOCAL_URL,
+	});
 
-    gulp.watch(SOURCE.styles, gulp.parallel('styles'));
-    gulp.watch(SOURCE.scripts, gulp.parallel('scripts')).on('change', browserSync.reload);
-    gulp.watch(SOURCE.images, gulp.parallel('images'));
+	gulp.watch(SOURCE.styles, gulp.parallel('styles')).on('change', browserSync.reload);
+	gulp.watch(SOURCE.scripts, gulp.parallel('scripts')).on('change', browserSync.reload);
+	gulp.watch(SOURCE.images, gulp.parallel('images')).on('change', browserSync.reload);
 
 });
 
 // Watch files for changes (without Browser-Sync)
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 
 	// Watch .scss files
 	gulp.watch(SOURCE.styles, gulp.parallel('styles'));
